3b219e345eb2c3606101a8edebc63c06030c9481
Unknown <ajbeers89@gmail.com>
2017-10-09 13:17:33 -0500

    inclass 10 9

--- likeButton.js
+++ likeButton.js
@@ -1,10 +1,19 @@
 
 
 $(function(){
-    $('.btn').click(function(e){
+    $("#like").click(function(e){
         let currentCount = $(this).next().text()
         currentCount = parseInt(currentCount)
         let newCount  = currentCount + 1
         $(this).next().text(" " + newCount) 
     })
+
+    $('#newTweet').click(function(e){
+        e.preventDefault()
+        console.log(this)
+        alert("test")
+    })
+
+
+
 });
