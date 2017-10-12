f70c689fb78485c0523fdceb48495c3fcc3b87cc
Unknown <ajbeers89@gmail.com>
2017-10-12 14:24:55 -0500

    changes

--- likeButton.js
+++ likeButton.js
@@ -1,22 +1,22 @@
 
 
-$(function(){
-    $("#like").click(function(e){
+$(function () {
+    $("#like").click(function (e) {
         let currentCount = $(this).next().text()
         currentCount = parseInt(currentCount)
-        let newCount  = currentCount + 1
-        $(this).next().text(" " + newCount) 
+        let newCount = currentCount + 1
+        $(this).next().text(" " + newCount)
     })
 
-    $('#newTweet').submit(function(e){
+    $('#newTweet').submit(function (e) {
         e.preventDefault()
         let tweet = this.elements.comment.value
         this.elements.comment.value = ""
         console.log(tweet)
 
-        $('#post').clone(true).insertAfter("#postedTweets")
-        
-            
+        $('#post').clone(true).insertBefore("#post")
+
+
 
         // alert("test")
     })
